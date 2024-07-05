349047364beba0350f4e3106483a0c50e1ba884a
Yevhenii Biletskyi <biletskijzena@protonmail.com>
2024-07-06 02:23:24 +0300

    Added cors and .env for connection string for db

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const db = require("./libs/db.js");
+const cors = require("cors");
 
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 
 app.get("/", async (req, res) => {
   try {
